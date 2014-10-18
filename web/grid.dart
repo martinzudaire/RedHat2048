c9a8fc80223926e7cc29810af8df22ea55569d99
Martin Zudaire <martinalejandrozudaire@gmail.com>
2014-10-18 10:47:37 -0300

    Coded grid.dart

--- web/grid.dart
+++ web/grid.dart
@@ -10,5 +10,30 @@ import 'Matrix.dart';
 
 class Grid {
   
+  Matrix _grid;
+  
+  Grid() {
+    _grid = new Matrix.fromZero(4, 4);
+  }
+  
+  Grid.fromJSON(String json) {
+    _grid = Matrix.parseMatrix(json, 4, 4);
+  }
+  
+  //Get & Set methods
+  
+  int getElement(int x, int y) {
+    if (x>=0 && x<4 && y>=0 && y<4) {
+      return _grid.getElement(x, y);
+    } else {
+      return 0;
+    }
+  }
+  
+  void setElement(int x, int y, int value) {
+    if (x>=0 && x<4 && y>=0 && y<4) {
+      _grid.setElement(value, x, y);
+    }
+  }
   
 }
